d467771bcf0d93264b843017410583a7da058845
Andreas Lydakis <andreas.lydakis@innotecuk.com>
2018-07-11 15:50:22 +0100

    readme and cleanup

--- README.md
+++ README.md
@@ -1,88 +1,52 @@
 ## imu_link
 
-The purpose of this ROS package is to link multiple IMUs to a mocap-like configuration.
-Nodes are separated to ***anchor points*** and ***imu points***. 
+The purpose of this ROS package is to link multiple IMUs to a mocap-like configuration based on offsets defined in
+config files. 
 
-***Anchor points*** are 
-fixed points in the world, that can be used to provide the global coordinates of the ***imu points***.
-
-***Imu points*** represent IMUs located in some part of your configuration (for example on an person's arm).
-The first of these must be connected to an ***anchor point*** in the TF tree. IMUs start
-with a known distance from the neighboring frames and an initial orientation with 
-regards to their parent frame.
-
-Each one subscribes to a ```sensor_msgs/Imu``` topic, and uses the published quaternions
+Each ```ImuPoint``` subscribes to a ```sensor_msgs/Imu``` topic, and uses the published quaternions
 to determine its position and orientation with regards to the parent frame.
 
 The configurations are described in ```.yaml``` files. Below is an example configuration:
 
 ```yaml
-anchor0:
-  sub_topic: ""
-  parent_frame: world
-  child_frame: hip
-  name: pelvis
-  anchor: 1
-  distance_from_parent: 0.15
-  distance_from_child: 0.15
-  offset_x: 0.0
-  offset_y: -0.15
-  offset_z: 0.0
-  offset_qx: 0.0
-  offset_qy: 0.0
-  offset_qz: 0.0
-  offset_qw: 0.0
-
 joint0:
-  sub_topic: imu
-  parent_frame: hip
+  sub_topic: /imu/data
+  parent_frame: world
+  name: hip
   child_frame: knee
-  name: thigh
-  anchor: 0
-  distance_from_parent: 0.3
-  distance_from_child: 0.3
   offset_x: 0.0
-  offset_y: -0.15
-  offset_z: 0.0
-  offset_qx: 0.0
-  offset_qy: 0.0
-  offset_qz: 0.0
-  offset_qw: 0.0
+  offset_y: 0.0
+  offset_z: -0.30
+  offset_roll: 0.0
+  offset_pitch: 0.0
+  offset_yaw: 0.0
 
 joint1:
-  sub_topic: imu/data
+  sub_topic: /imu/
   parent_frame: knee
+  name: knee2
   child_frame: ankle
-  name: tibia
-  anchor: 0
-  distance_from_parent: 0.3
-  distance_from_child: 0.3
   offset_x: 0.0
-  offset_y: -0.15
-  offset_z: 0.0
-  offset_qx: 0.0
-  offset_qy: 0.0
-  offset_qz: 0.0
-  offset_qw: 0.0
+  offset_y: 0.0
+  offset_z: -0.30
+  offset_roll: 0.0
+  offset_pitch: 0.0
+  offset_yaw: 0.0
 ```
 
-This file describes an anchor point located in the hip of a person, and two IMUs, located in mid thigh and mid tibia.
- The resulting tree is:
+This file describes a configuration for two IMUs located on the hip and knee joint:
 
-> world -> {- pelvis -} -> hip -> {+ thigh +} -> knee -> {+ tibia +} -> ankle
+> world -> {+ thigh +} -> knee -> {+ knee2 +} -> angle
 
-Where red nodes are anchor points, green nodes are IMU points and black nodes are mostly decorative TFs for visualization.
+Where green nodes are IMU points and black nodes are mostly decorative TFs for visualization.
 
 Below is a description of the different parameters for each point:
 - **sub_topic:** The topic that the corresponding IMU publishes to (not needed for anchor points)
 - **parent_frame:** The frame that acts as the parent frame for the ```parent_frame -> name``` transform
 - **child_frame:** The frame that acts as the child frame for the ```name -> child_frame``` transform
 - **name:** The name of the IMU/Anchor point, located between the parent and child frame.
-- **anchor:** 1 if the point is an anchor point,  0 otherwise
-- **distance_from_parent:** The straight line distance between ```parent_frame``` and the point 
-- **distance_from_child:** The straight line distance between ```child_frame``` and the point 
-- **offset_x/y/z:** The starting x,y,z offset between ```parent_frame``` and the point 
-- **offset_qx/qy/qz/qw:** The starting orientation (as a quaternion [qx, qy, qz, qw]) offset between ```parent_frame``` and the point
+- **offset_x/y/z:** The starting x,y,z offset between ```name``` and ```child_frame```
+- **offset_roll/pitch/yaw:** The starting orientation (as a quaternion [qx, qy, qz, qw]) offset between ```name``` and ```child_frame```
  
 ### Execution
 The sample launch file launches a Phidget IMU and a Razor 9Dof IMU in the configuration described above
--- scripts/JointPublisher.py
+++ scripts/JointPublisher.py
@@ -1,101 +1,63 @@
 #!/usr/bin/env python
 
-import math
+
 import rospy
-import tf
-import tf2_ros
-from AnchorPoint import AnchorPoint
 from ImuPoint import ImuPoint
-from geometry_msgs.msg import TransformStamped
-from geometry_msgs.msg import Quaternion
-from geometry_msgs.msg import QuaternionStamped
 
 decor = "#----------------------------------------------#"
+
+
+def read_joints_from_params():
+    joints = {}
+    count = 0
+    rospy.loginfo(rospy.has_param("/joint" + str(count) + "/name"))
+    while True:
+        if rospy.has_param("/joint" + str(count) + "/name"):
+            params = {"sub_topic": rospy.get_param("/joint" + str(count) + "/sub_topic"),
+                      "parent_frame": rospy.get_param("/joint" + str(count) + "/parent_frame"),
+                      "child_frame": rospy.get_param("/joint" + str(count) + "/child_frame"),
+                      "name": rospy.get_param("/joint" + str(count) + "/name"),
+                      "offset_x": float(rospy.get_param("/joint" + str(count) + "/offset_x")),
+                      "offset_y": float(rospy.get_param("/joint" + str(count) + "/offset_y")),
+                      "offset_z": float(rospy.get_param("/joint" + str(count) + "/offset_z")),
+                      "offset_roll": float(rospy.get_param("/joint" + str(count) + "/offset_roll")),
+                      "offset_pitch": float(rospy.get_param("/joint" + str(count) + "/offset_pitch")),
+                      "offset_yaw": float(rospy.get_param("/joint" + str(count) + "/offset_yaw"))}
+            print(params)
+            joints[params["name"]] = ImuPoint(name_=params["name"], child_frame_=params["child_frame"],
+                                              parent_frame_=params["parent_frame"], sub_topic_=params["sub_topic"],
+                                              ipitch_=params["offset_pitch"], iroll_=params["offset_roll"],
+                                              iyaw_=params["offset_yaw"], ix_=params["offset_x"],
+                                              iy_=params["offset_y"], iz_=params["offset_z"])
+            count += 1
+        else:
+            break
+
+    rospy.loginfo(decor)
+    rospy.loginfo("Found " + str(count) + " joints")
+    rospy.loginfo(joints)
+    rospy.loginfo(decor)
+    return joints
 
 
 class JointPublisher:
 
     def __init__(self):
         rospy.init_node("imu_joint_publisher")
-        r = rospy.Rate(100)
-        anchors = {}
-        joints = {}
-        count = 0
-        while True:
-            try:
-                if rospy.has_param("/anchor" + str(count) + "/name"):
-                    params = {}
-                    params["sub_topic"] = rospy.get_param("/anchor" + str(count) + "/sub_topic")
-                    params["parent_frame"] = rospy.get_param("/anchor" + str(count) + "/parent_frame")
-                    params["child_frame"] = rospy.get_param("/anchor" + str(count) + "/child_frame")
-                    params["name"] = rospy.get_param("/anchor" + str(count) + "/name")
-                    params["anchor"] = int(rospy.get_param("/anchor" + str(count) + "/anchor"))
-                    params["distance_from_parent"] = float(
-                        rospy.get_param("/anchor" + str(count) + "/distance_from_parent"))
-                    params["distance_from_child"] = float(
-                        rospy.get_param("/anchor" + str(count) + "/distance_from_child"))
+        self.r = rospy.Rate(5)
+        self.joints = read_joints_from_params()
+        if not self.joints:
+            rospy.logerr("No joints detected, exiting")
+            exit(-1)
 
-                    params["offset_x"] = float(rospy.get_param("/anchor" + str(count) + "/offset_x"))
-                    params["offset_y"] = float(rospy.get_param("/anchor" + str(count) + "/offset_y"))
-                    params["offset_z"] = float(rospy.get_param("/anchor" + str(count) + "/offset_z"))
-
-                    params["offset_qx"] = float(rospy.get_param("/anchor" + str(count) + "/offset_qx"))
-                    params["offset_qy"] = float(rospy.get_param("/anchor" + str(count) + "/offset_qy"))
-                    params["offset_qz"] = float(rospy.get_param("/anchor" + str(count) + "/offset_qz"))
-                    params["offset_qw"] = float(rospy.get_param("/anchor" + str(count) + "/offset_qw"))
-
-                    anchors[params["name"]] = ImuPoint(params["parent_frame"], params["name"], params)
-                    count += 1
-                else:
-                    break
-            except:
-                break
-        rospy.loginfo(decor)
-        rospy.loginfo("Found " + str(count) + " anchors")
-        rospy.loginfo(anchors)
-        count = 0
-        while True:
-            try:
-                if rospy.has_param("/joint" + str(count) + "/name"):
-                    params = {}
-                    params["sub_topic"] = rospy.get_param("/joint" + str(count) + "/sub_topic")
-                    params["parent_frame"] = rospy.get_param("/joint" + str(count) + "/parent_frame")
-                    params["child_frame"] = rospy.get_param("/joint" + str(count) + "/child_frame")
-                    params["name"] = rospy.get_param("/joint" + str(count) + "/name")
-                    params["anchor"] = int(rospy.get_param("/joint" + str(count) + "/anchor"))
-                    params["distance_from_parent"] = float(
-                        rospy.get_param("/joint" + str(count) + "/distance_from_parent"))
-                    params["distance_from_child"] = float(
-                        rospy.get_param("/joint" + str(count) + "/distance_from_child"))
-
-                    params["offset_x"] = float(rospy.get_param("/joint" + str(count) + "/offset_x"))
-                    params["offset_y"] = float(rospy.get_param("/joint" + str(count) + "/offset_y"))
-                    params["offset_z"] = float(rospy.get_param("/joint" + str(count) + "/offset_z"))
-
-                    params["offset_qx"] = float(rospy.get_param("/joint" + str(count) + "/offset_qx"))
-                    params["offset_qy"] = float(rospy.get_param("/joint" + str(count) + "/offset_qy"))
-                    params["offset_qz"] = float(rospy.get_param("/joint" + str(count) + "/offset_qz"))
-                    params["offset_qw"] = float(rospy.get_param("/joint" + str(count) + "/offset_qw"))
-                    
-                    joints[count] = ImuPoint(params["parent_frame"], params["name"], params)
-                    rospy.loginfo(params)
-                    count += 1
-                    print(params)
-                else:
-                    rospy.loginfo("Done reading parameters")
-                    break
-            except:
-                break
-        rospy.loginfo("Found " + str(count) + " joints")
-        rospy.loginfo(joints)
-        rospy.loginfo(decor)
+    def run(self):
+        rospy.logerr("Joint Publisher Spinning")
         while not rospy.is_shutdown():
-            for anchor_id, anchor in anchors.iteritems():
-                anchor.update()
-            for joint_id, joint in joints.iteritems():
+            for joint_id, joint in self.joints.iteritems():
                 joint.update()
-            r.sleep()
+            self.r.sleep()
 
 
 if __name__ == "__main__":
-    JointPublisher()
+    jp = JointPublisher()
+    jp.run()
